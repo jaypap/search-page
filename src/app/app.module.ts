9434e6a6a68a55a6a32bc0d35777e273a7c31532
jaypap <gpapadakis1982@gmail.com>
2019-12-22 23:26:16 +0200

    create search-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+// Components
 import { AppComponent } from './app.component';
+// Modules
+import { SearchPageModule } from './search-page/search-page.module';
 
+
+
+import { ROUTES } from './app.routes';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SearchPageModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, {
+      useHash: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
